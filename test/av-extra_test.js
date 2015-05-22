cab37f0b97c4dc93968c9a73fb6d53e17dd2a9a0
wchen <sdjcw13@gmail.com>
2015-05-22 17:37:22 +0800

    增加 httpRequest 的测试

---
+++ test/av-extra_test.js
@@ -0,0 +1,18 @@
+var should = require('should'),
+    AV = require('../lib/av-extra');
+
+describe('av-extra', function() {
+  it('httpRequest', function(done) {
+    AV.Cloud.httpRequest({
+      url: 'https://www.bing.com/search',
+      params: { q : 'leancloud怎么样' },
+      success: function(httpResponse) {
+        httpResponse.status.should.equal(200);
+        done();   
+      },
+      error: function(httpResponse) {
+        throw httpResponse.text
+      }
+    });
+  });
+});
