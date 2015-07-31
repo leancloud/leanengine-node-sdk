0228da4dcdb4b1cd00c14f418fba98b8f3d2e232
wchen <sdjcw13@gmail.com>
2015-07-31 14:42:10 +0800

    修改 AV.Cloud.httpRequest 的测试

--- test/av-extra_test.js
+++ test/av-extra_test.js
@@ -2,14 +2,53 @@
 var should = require('should'), // jshint ignore:line
     AV = require('../lib/av-extra');
 
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/hello', function (req, res) {
+  res.send('Hello, ' + req.query.name);
+});
+
+app.post('/hello', function (req, res) {
+  res.send('Hello, ' + req.body.name);
+});
+
+var server = app.listen(3333, function () {});
+
+after(function() {
+  server.close();
+});
+
 describe('av-extra', function() {
   it('httpRequest', function(done) {
     AV.Cloud.httpRequest({
-      url: 'https://www.bing.com/search',
-      params: { q : 'leancloud怎么样' },
-      success: function(httpResponse) {
-        httpResponse.status.should.equal(200);
-        done();   
+      url: 'http://localhost:3333/hello',
+      params: { name : '张三' },
+      success: function(res) {
+        res.status.should.equal(200);
+        res.text.should.equal('Hello, 张三');
+        done();
+      },
+      error: function(httpResponse) {
+        throw httpResponse.text;
+      }
+    });
+  });
+
+  it('httpRequest', function(done) {
+    AV.Cloud.httpRequest({
+      url: 'http://localhost:3333/hello',
+      method: 'POST',
+      body: {
+        name: "张三"
+      },
+      success: function(res) {
+        res.status.should.equal(200);
+        res.text.should.equal('Hello, 张三');
+        done();
       },
       error: function(httpResponse) {
         throw httpResponse.text;
