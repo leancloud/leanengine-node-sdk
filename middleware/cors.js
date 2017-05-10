527d2f3290cb3f33ace0d7fd004aae4b91a69376
jysperm <jysperm@gmail.com>
2017-05-10 18:05:10 +0800

    :heavy_plus_sign: Use leancloud-cors-headers

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,3 +1,5 @@
+const leancloudHeaders = require('leancloud-cors-headers');
+
 module.exports = function() {
   return function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
@@ -5,8 +7,8 @@ module.exports = function() {
     if (req.method.toLowerCase() === 'options') {
       res.statusCode = 200;
       res.setHeader('Access-Control-Max-Age','86400');
-      res.setHeader('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-      res.setHeader('Access-Control-Allow-Headers', 'X-LC-Id, X-LC-Key, X-LC-Session, X-LC-Sign, X-LC-Prod, X-LC-UA, X-Uluru-Application-Key, X-Uluru-Application-Id, X-Uluru-Application-Production, X-Uluru-Client-Version, X-Uluru-Session-Token, X-AVOSCloud-Application-Key, X-AVOSCloud-Application-Id, X-AVOSCloud-Application-Production, X-AVOSCloud-Client-Version, X-AVOSCloud-Session-Token, X-AVOSCloud-Super-Key, X-Requested-With, Content-Type, X-AVOSCloud-Request-sign, X-LC-Hook-Key');
+      res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, DELETE, OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', leancloudHeaders);
       res.setHeader('Content-Length', 0);
       res.end();
     } else {
