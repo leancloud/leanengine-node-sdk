367f06a4095eeeabd2d436bef8355202a70fe94f
Jang Rush <weakish@gmail.com>
2020-04-14 16:53:59 +0800

    feat: https redirect with avosapps.us

--- middleware/https-redirect.js
+++ middleware/https-redirect.js
@@ -5,7 +5,7 @@ var endsWith = require('../lib/utils').endsWith;
 module.exports = function(AV) {
   return function() {
     return function(req, res, next) {
-      if ((AV.Cloud.__prod || endsWith(req.headers.host, '.leanapp.cn')) && (!req.secure)) {
+      if ((AV.Cloud.__prod || endsWith(req.headers.host, '.leanapp.cn') || endsWith(req.headers.host, '.avosapps.us')) && (!req.secure)) {
         const url = `https://${req.headers.host}${req.originalUrl || req.url}`;
 
         res.statusCode = 302;
