23668ce36aa385e6b41397421f5f8e3aae070794
jysperm <jysperm@gmail.com>
2021-07-20 14:50:38 +0800

    :white_check_mark: Add test for leancloud-storage@4

--- test/fixtures/app-info.js
+++ test/fixtures/app-info.js
@@ -5,7 +5,8 @@ const appInfo = {
   appKey: '3xjj1qw91cr3ygjq9lt0g8c3qpet38rrxtwmmp0yffyoy2t4',
   masterKey: '3v7z633lzfec9qzx8sjql6zimvdpmtwypcchr2gelu5mrzb0',
   hookKey: 'qlkcRqv9v5J5A11Byr3mzori',
-  sessionTokenAdmin: 'w2jrtkbehp38otqmhbqu7ybs9'
+  sessionTokenAdmin: 'w2jrtkbehp38otqmhbqu7ybs9',
+  serverURL: 'https://4h2h4okw.lc-cn-n1-shared.com'
 };
 
 AV.init(appInfo);
